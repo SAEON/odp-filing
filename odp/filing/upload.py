38cedad67a3a5b5b0fc4b04e406c24ff7bb0566d
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2024-11-05 21:44:40 +0200

    Implement zipfile unpacking

--- odp/filing/upload.py
+++ odp/filing/upload.py
@@ -1,22 +1,85 @@
-from sanic import Blueprint, HTTPResponse, Request, SanicException, text
+import shutil
+from pathlib import Path
+
+from sanic import Blueprint, HTTPResponse, json, Request, SanicException
+from sanic.request import File
+from werkzeug.utils import secure_filename
 
 from odp.lib.filestore import Filestore, FilestoreError
 
 bp = Blueprint('upload', url_prefix='/upload')
 
 
-@bp.post('/<path:path>')
-async def upload_file(request: Request, path: str) -> HTTPResponse:
-    if not (file := request.files.get('file')):
-        raise SanicException("Expecting upload 'file'", status_code=400)
+@bp.before_server_start
+async def register_unpack_formats(*_):
+    """Ensure only zip is registered. The tar formats come
+    with security considerations that are complex to address."""
+    unpack_formats = shutil.get_unpack_formats()
+    for fmt, _, _ in unpack_formats:
+        if fmt != 'zip':
+            shutil.unregister_unpack_format(fmt)
 
-    if not (sha256 := request.args.get('sha256')):
-        raise SanicException("Expecting arg 'sha256'", status_code=400)
 
-    filestore = Filestore(request.app.config.ODP_UPLOAD_DIR)
+def _get_file(request: Request, arg: str) -> File:
+    if not (file := request.files.get(arg)):
+        raise SanicException(f"Expecting upload '{arg}'", status_code=400)
+    return file
+
+
+def _get_arg(request: Request, arg: str, default=None) -> str:
+    if (val := request.args.get(arg)) is None and default is None:
+        raise SanicException(f"Expecting arg '{arg}'", status_code=400)
+    return val
+
+
+def _get_filestore(request: Request) -> Filestore:
+    return Filestore(request.app.config.ODP_UPLOAD_DIR)
+
+
+@bp.put('/<folder:path>')
+async def upload_file(request: Request, folder: str) -> HTTPResponse:
+    """Upload a file to `folder`, relative to the filestore base directory.
+
+    If unpack is true, the file is unzipped into `folder`.
+
+    The response is a JSON object whose keys are file paths relative
+    to the filestore's base directory, for every file that has been
+    created/updated. The corresponding values are length 2 arrays
+    of [file size, file hash].
+    """
+    file = _get_file(request, 'file')
+    filename = _get_arg(request, 'filename')
+    sha256 = _get_arg(request, 'sha256')
+    unpack = _get_arg(request, 'unpack', False)
+    filestore = _get_filestore(request)
+
+    if '..' in folder:
+        raise SanicException("'..' not allowed in folder", status_code=422)
+
+    if Path(folder).is_absolute():
+        raise SanicException('folder must be relative', status_code=422)
+
+    if not (filename := secure_filename(filename)):
+        raise SanicException('invalid filename', status_code=422)
+
+    if unpack:
+        filename = filename.lower()
+        if Path(filename).suffix != '.zip':
+            raise SanicException('unpack is supported only for zip files', status_code=422)
+
     try:
-        filestore.put(file.body, path, sha256)
+        if unpack:
+            result = {
+                finfo.relpath: [finfo.size, finfo.sha256]
+                for finfo in filestore.unpack(folder, filename, file.body, sha256)
+            }
+        else:
+            finfo = filestore.put(folder, filename, file.body, sha256)
+            result = {
+                finfo.relpath: [finfo.size, finfo.sha256]
+            }
+
     except FilestoreError as e:
         raise SanicException(str(e), status_code=422) from e
 
-    return text('File uploaded', status=201)
+    return json(result)
--- odp/lib/filestore.py
+++ odp/lib/filestore.py
@@ -1,6 +1,16 @@
 import hashlib
+import os
+import shutil
+from collections import namedtuple
+from contextlib import contextmanager
 from os import PathLike
 from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import ContextManager
+
+FileInfo = namedtuple('FileInfo', (
+    'relpath', 'size', 'sha256'
+))
 
 
 class FilestoreError(Exception):
@@ -8,39 +18,103 @@ class FilestoreError(Exception):
 
 
 class Filestore:
-    """File management for a Linux-based file system."""
+    """File storage controller.
+
+    Incoming data are always written first to temporary directories,
+    to mitigate against partial failures, filesystem errors and
+    denial-of-service attacks.
+
+    If a destination path already exists (whether a file or dir),
+    an exception is raised rather than silently overwriting it.
+    """
 
     def __init__(self, base_dir: str | PathLike):
         self.base_dir = Path(base_dir)
 
-    def put(self, data: bytes, path: str | PathLike, sha256: str) -> None:
-        upload_path = self.base_dir / path
+    def put(self, folder: str, filename: str, data: bytes, sha256: str) -> FileInfo:
+        """Write file data to filename within folder relative to the base dir,
+        and verify against sha256.
+
+        Return tuple(relpath, size, sha256).
+        """
+        relpath = Path(folder) / filename
+        destpath = self.base_dir / relpath
+
+        if destpath.exists():
+            raise FilestoreError(f'Destination path {relpath} already exists')
+
+        with self._save_to_tmpdir(filename, data, sha256) as tmpfile:
+            self._move_to_dest(tmpfile, destpath)
+
+        return FileInfo(
+            relpath, destpath.stat().st_size, sha256
+        )
+
+    def unpack(self, folder: str, filename: str, data: bytes, sha256: str) -> list[FileInfo]:
+        """Unpack zipped file data into folder relative to the base dir.
+
+        The zip file is verified against sha256 and discarded.
+
+        Return a list of tuple(relpath, size, sha256) for all unpacked files.
+        """
+        with self._save_to_tmpdir(filename, data, sha256) as tmpfile:
+            unpack_dir = tmpfile.parent / tmpfile.stem
+            try:
+                shutil.unpack_archive(tmpfile, unpack_dir / folder)
+            except (OSError, ValueError) as e:
+                raise FilestoreError(f'Error unpacking zip file: {e}') from e
+
+            files = []
+            errors = []
+            for dirpath, dirnames, filenames in os.walk(unpack_dir):  # TODO: use Path.walk() in Python 3.12
+                for filename in filenames:
+                    srcpath = Path(dirpath) / filename
+                    relpath = srcpath.relative_to(unpack_dir)
+                    destpath = self.base_dir / relpath
+                    if destpath.exists():
+                        errors += [f'Destination path {relpath} already exists']
+                        continue
+                    with open(srcpath, 'rb') as f:
+                        filehash = hashlib.sha256(f.read()).hexdigest()
+                    files += [FileInfo(
+                        relpath, srcpath.stat().st_size, filehash
+                    )]
+
+            if errors:
+                raise FilestoreError(errors)
+
+            for finfo in files:
+                self._move_to_dest(unpack_dir / finfo.relpath, finfo.relpath)
+
+            return files
+
+    @contextmanager
+    def _save_to_tmpdir(self, filename: str, data: bytes, sha256: str) -> ContextManager[Path]:
+        """Create a temporary directory, write file data to /tmp/tmpdir/filename,
+        verify against sha256, and yield the temporary file path. The temporary
+        directory is deleted upon exiting the context manager."""
+        with TemporaryDirectory() as tmpdir:
+            try:
+                with open(tmpfile := Path(tmpdir) / filename, 'wb') as f:
+                    f.write(data)
+            except OSError as e:
+                raise FilestoreError(f'Error saving uploaded file: {e}') from e
+
+            with open(tmpfile, 'rb') as f:
+                if sha256 != hashlib.sha256(f.read()).hexdigest():
+                    raise FilestoreError('Error uploading file: checksum verification failed')
+
+            yield tmpfile
+
+    def _move_to_dest(self, srcpath: Path, relpath: Path) -> None:
+        destpath = self.base_dir / relpath
+        try:
+            if not destpath.parent.exists():
+                destpath.parent.mkdir(mode=0o755, parents=True, exist_ok=True)
+        except OSError as e:
+            raise FilestoreError(f'Error creating directory at {relpath.parent}: {e}') from e
 
         try:
-            upload_path.parent.mkdir(mode=0o755, parents=True, exist_ok=True)
+            shutil.move(srcpath, destpath)
         except OSError as e:
-            raise FilestoreError(
-                f'Error creating directory at {Path(path).parent}: {e}'
-            ) from e
-
-        try:
-            with open(upload_path, 'wb') as f:
-                f.write(data)
-        except OSError as e:
-            raise FilestoreError(
-                f'Error creating file at {path}: {e}'
-            ) from e
-
-        with open(upload_path, 'rb') as f:
-            if sha256 != hashlib.sha256(f.read()).hexdigest():
-                raise FilestoreError(
-                    f'Error creating file at {path}: checksum verification failed'
-                )
-
-        try:
-            with open(f'{upload_path}.sha256', 'wt') as f:
-                f.write(f'{sha256} {upload_path.name}\n')
-        except OSError as e:
-            raise FilestoreError(
-                f'Error creating checksum file at {path}.sha256: {e}'
-            ) from e
+            raise FilestoreError(f'Error creating file at {relpath}: {e}') from e
